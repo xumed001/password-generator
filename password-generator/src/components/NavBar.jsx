cf1af9f8f2f0427ea52a3625ed7f15ed62fcdb7a
Sumed Tamang <xumed001@gmail.com>
2023-10-26 00:11:35 -0500

    initial commit

---
+++ password-generator/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+//
+//
+
+function NavBar() {
+    return (
+        <>
+        <header>
+            <nav className="header-nav">
+                <h1>Password Generator</h1>
+                <p>Generatre secure passwords </p>
+            </nav>
+        </header>
+        </>
+    )
+}
+
+export default NavBar
