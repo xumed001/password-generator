43c9c1af70031efa1f0137d9b0955f94c3a66eca
Sumed Tamang <xumed001@gmail.com>
2023-12-06 21:01:30 -0600

    fixed footer link, minor CSS updates

--- password-generator/src/components/Footer.jsx
+++ password-generator/src/components/Footer.jsx
@@ -1,17 +1,25 @@
 //
 //
 import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
+// import { FaLinkedin } from "react-icons/fa";
 
 function Footer() {
     return (
         <footer>
             <p>
-                <FaGithub />GitHub
+                <FaGithub /> 
+                <a 
+                    className="github-link"
+                    href="https://github.com/xumed001/password-generator"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                GitHub
+                </a>
             </p> 
-            <p>
+            {/* <p>
                 <FaLinkedin />Linkedin
-            </p>
+            </p> */}
         </footer>
     )
 }
